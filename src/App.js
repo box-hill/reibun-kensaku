35396b2da328267e6e6e741493a585ab1152dfba
aaron <aaronlim314@gmail.com>
2021-10-24 15:43:59 +1100

    Installed and tested Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 
 import './App.css';
 
+import firebase from "./firebase";
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+
 function App() {
+  const db = firebase.firestore(); // initialise your database
+
+  function testWrite(){
+    // const search = document.querySelector('#input-search');
+    // const searchString = search.value; 
+
+    // const user = firebase.auth().currentUser;
+    // const uniqueId = user.uid;
+    // console.log(uniqueId);
+    
+    const db = firebase.firestore(); // initialise your database
+    db.collection('users').doc('test').collection('searches').add({
+        //text: searchString,
+        text: 'abcdefg'
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error addin doc: ", error);
+    })
+
+  }
+
+  testWrite();
+
   return (
-    <div> Initiating file test
+    <div> Initiating file test 123 123
 
     </div>
   );
