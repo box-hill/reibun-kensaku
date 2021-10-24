a78eb4ab8265f05a8d21c914c2c3b128b4757840
aaron <aaronlim314@gmail.com>
2021-10-24 20:09:58 +1100

    Added save search history functionality

--- src/App.js
+++ src/App.js
@@ -9,39 +9,97 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { faQuestionCircle, faSearch } from '@fortawesome/free-solid-svg-icons';
 
+import React, { useState, useEffect } from 'react';
+
 function App() {
-  const db = firebase.firestore(); // initialise your database
 
-  function testWrite(){
-    // const search = document.querySelector('#input-search');
-    // const searchString = search.value; 
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [searchInput, setSearchInput] = useState(undefined);
+  const [justSearched, setJustSearched] = useState(false);
 
-    // const user = firebase.auth().currentUser;
-    // const uniqueId = user.uid;
-    // console.log(uniqueId);
+  // check if user is logged in
+  function checkUser(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if(user) {
+            setLoggedIn(true);
+            console.log('user is logged in ');
+        } else { 
+            setLoggedIn(false);
+            console.log('not signed in');
+        }
+    })
+  }
+
+  useEffect(() => {
+    checkUser();
+  }, [])
+  
+  function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            const user = result.user;
+            console.log(user)
+        })
+        .catch(console.log);
+  }
+
+  function logUserOut(){
+    const auth = firebase.auth();    
+    auth.signOut();
+  }
+
+  function search(e) {
+    e.preventDefault();
+
+    // only allow 1 search a second
+    if( justSearched ) return; 
+    setJustSearched(true);
+    setTimeout(() => setJustSearched(false), 1000);
+
+    const searchString = searchInput; 
+    console.log('search is: ', searchString);
     
-    const db = firebase.firestore(); // initialise your database
-    db.collection('users').doc('test').collection('searches').add({
-        //text: searchString,
-        text: 'abcdefg'
-    })
-    .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch((error) => {
-        console.error("Error addin doc: ", error);
-    })
+    /* ADD: code for google search api goes here */
+    // string manipulation can go somewhere else.
+    
+    // check if user is logged in, and then write to firestore to save user's search
+    if( loggedIn ) {
+        const user = firebase.auth().currentUser;
+        const uniqueId = user.uid;
 
+        const date = new Date();
+        const time = date.getTime();
+        
+        const db = firebase.firestore(); // initialise your database
+        db.collection('users').doc(uniqueId).collection('searches').add({
+            text: searchString,
+            searchedOn: time,
+        })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id, "User ID: ", uniqueId);
+        })
+        .catch((error) => {
+            console.error("Error addin doc: ", error);
+        })
+      /* ADD: code to delete once searches get past 100*/
+    }
+
+  }
+
+  function handleChange(e) {
+    console.log(e.target.value);
+    setSearchInput(e.target.value);
   }
 
   return (
     <div>
       <Navbar>
         <div className="nav-search">
-          <NavSearchBar/>
+          <NavSearchBar search={search} handleChange={handleChange}/>
         </div>
         <div className="nav-items">
-          <NavItem text="Sign in" icon={faGoogle}></NavItem>
+          {loggedIn ? <NavItem text="Sign out" icon={faGoogle} onClick={logUserOut}></NavItem> : <NavItem text="Sign in" icon={faGoogle} onClick={googleLogin}></NavItem>}
           <NavItem text="About" icon={faQuestionCircle}></NavItem>
         </div>
       </Navbar>
@@ -60,21 +118,20 @@ function Navbar(props) {
 function NavItem(props) {
   return (
     <li className="nav-item">
-      
-      <button herf="#" className="text-button">
+      <button herf="#" className="text-button" onClick={props.onClick}>
       <FontAwesomeIcon icon={props.icon}/> {props.text}
       </button>
     </li>
   );
 }
 
-function NavSearchBar() {
+function NavSearchBar(props) {
   return (
-    <div>
-    <input type="text" maxLength="25" placeholder="Type some japanese here" className="nav-search-bar">
+    <form>
+    <input type="text" maxLength="25" placeholder="Type some japanese here" className="nav-search-bar" id="search-input" onChange={props.handleChange}>
     </input>
-    <button className="nav-search-bar-button"><FontAwesomeIcon icon={faSearch}/>Search</button>
-    </div>
+    <button className="nav-search-bar-button" onClick={props.search}><FontAwesomeIcon icon={faSearch}/>Search</button>
+    </form>
   );
 }
 
