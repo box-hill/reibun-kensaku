212685e0f056fa50c5560bbea7472c4941afd13d
aaron <aaronlim314@gmail.com>
2021-10-27 22:30:50 +1100

    Added number of results feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Card from './components/Card';
 
 import firebase from "./firebase";
 import 'firebase/compat/firestore';
+import { query, orderBy, limit } from "firebase/firestore";  
 import 'firebase/compat/auth';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -19,7 +20,7 @@ function App() {
 
   const [loggedIn, setLoggedIn] = useState(false);
   const [searchInput, setSearchInput] = useState('');
-  const [searchInputParsed, setSearchInputParsed] = useState('');
+  const [numberResults, setNumberResults] = useState(undefined);
   const [justSearched, setJustSearched] = useState(false);
 
   const [parsedResults, setParsedResults] = useState([]);
@@ -63,18 +64,24 @@ function App() {
       const db = firebase.firestore(); // initialise your database
       const docRef = db.collection('users').doc(uniqueId).collection('searches');
 
-      docRef.get()
+      docRef.orderBy("searchedOn", "desc").get()
       .then((querySnapshot) => {
+        console.log('query', querySnapshot);
+        let historyArray = [];
         querySnapshot.forEach((doc) => {
-          //console.log(doc.data().text);
-          setHistory(prevData => [...prevData, doc.data().text]);
+          console.log(doc.data());
+          historyArray.push(doc.data());
+          //setHistory(prevData => [...prevData, doc.data().text]);
         })
+        setHistory(historyArray);
+        console.log(historyArray);
       })
       .catch((error => {
         console.log("Error getting documents: ", error);
       }))
       console.log('Retrieved History: ', history);
       
+
     }
   }
 
@@ -112,122 +119,116 @@ function App() {
     setLoadingSearch(true); // mount loader
 
     let searchString = searchInput.replace(/\s/g, ''); // remove all spaces from user's search
-
-    setSearchInputParsed(searchString);
     console.log('search is: ', searchString);
-
+    let formattedNumberResults = 0;
     let url = `https://www.googleapis.com/customsearch/v1?key=${REACT_APP_GOOGLE_API}&cx=22519e5637b61b1c8&q=\"${searchString}"`;
     fetch(url, {mode: 'cors'})
     .then((response) => {
         return response.json();
     })
     .then((response) => {
-        console.log('search response: ', response);
-
-        // The results with <b> tags contain the search term
-        // If neither the htmlSnippet or htmlTitle contains this, then the hit is likely a bad one
-        function parseResults(resultsObj) {
-          // replace all <b>...</b> tags with the keyword searched, &nbsp HTML entities; 
-          function replaceTagInString(inputString){
-            const regexTag = new RegExp("<b>.*</b>", "g");
-            const regexHTML = new RegExp("&nbsp;", "g");
-            let outputString = inputString.replaceAll(regexTag, searchString);
-            outputString = outputString.replaceAll(regexHTML, "…");         
-            return outputString;
-          }
-          // removes the result's date if it exists in the result
-          function removeDateInString(inputString){
-            const regex = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-]\d{2}/g;
-            const outputString = inputString.replaceAll(regex, '');
-            return outputString;
-          }
-          // extract sentence by estimating the start and end markers
-          function extractSentence(sentence){
-            let hitIndex = sentence.indexOf(searchString);
-            function findIndexMarker(string, markers, hitIndex, start=true, punctuation=false){
-              // to find the start of the sentence, find the latest marker before hitIndex
-              if(start){
-                let latestStartIndex = 0;
-                markers.forEach((marker) => {
-                  let markerIndex = string.indexOf(marker) + 1; // don't include the punctuation at the start
-                  latestStartIndex = ((markerIndex > latestStartIndex) && (markerIndex < hitIndex)) ? markerIndex : latestStartIndex;
-                });
-                return latestStartIndex;
-              }
-              else{ // find the earliest end of the sentence that is after the hitIndex
-                let earliestEndIndex = string.length;
-                markers.forEach((marker) => {
-                  let markerIndex = punctuation ? string.indexOf(marker) + 1 : string.indexOf(marker); // include the punctuation if needed
-                  earliestEndIndex = ((markerIndex < earliestEndIndex) && (markerIndex > hitIndex)) ? markerIndex : earliestEndIndex;
-                });
-                return earliestEndIndex;
-              }
-            }
-            let startMarkers = [".", ";", "。", "…", "？", "?", "|","｜","！","!","→"];
-            let endMarkers = [";","|","｜","→","【", "&"];
-            let endMarkersPunc= [".", , "。", "…", "？", "?","！","!"];
-
-            let startIndex = findIndexMarker(sentence, startMarkers, hitIndex, true);
-            let endIndex = findIndexMarker(sentence, endMarkers, hitIndex, false);
-            let endIndexPunc = findIndexMarker(sentence, endMarkersPunc, hitIndex, false, true);
-
-            endIndex = (endIndexPunc < endIndex) ? endIndexPunc : endIndex;
-            return {
-              text: sentence.slice(startIndex, endIndex), 
-              start: sentence.slice(startIndex, hitIndex),
-              searchedPhrase: searchString,
-              end: sentence.slice(hitIndex + searchString.length, endIndex)
-            };
-          }
-          let outputArray = [];
-          // Add text and link property from our results
-          resultsObj.forEach(resultObj => {
-            let sentenceString = '';
-            if(resultObj.htmlSnippet.includes("<b>")){
-              sentenceString = replaceTagInString(resultObj.htmlSnippet);
-                //outputArray = [...outputArray, {text: replaceTagInString(resultObj.htmlSnippet), link: resultObj.link}];
-            }
-            else if(resultObj.htmlTitle.includes("<b>")){
-              sentenceString = replaceTagInString(resultObj.htmlTitle);
-            }
-            else {
-              sentenceString = null;
-              outputArray = [...outputArray, {text: null}]
-              return;
-            }              
-            // Remove post date in sentence and extract sentence
-            sentenceString = removeDateInString(sentenceString);
-            let extractedObject = extractSentence(sentenceString);
-
-            // once w ehaver our sentence, we can split into 3 parts, phrase 1, bold keyword, phrase 2
-            const fullSentence = extractedObject.text
-            const start = extractedObject.start;
-            const searchedPhrase = extractedObject.searchedPhrase;
-            const end = extractedObject.end;
-
-            outputArray = [...outputArray, {
-              text: fullSentence, 
-              start: start, 
-              searchedPhrase: searchedPhrase, 
-              end: end, 
-              link: resultObj.link
-            }];
-          });
-          console.log(outputArray);          
-          return outputArray;
+      console.log('search response: ', response);
+      // The results with <b> tags contain the search term
+      // If neither the htmlSnippet or htmlTitle contains this, then the hit is likely a bad one
+      function parseResults(resultsObj) {
+        // replace all <b>...</b> tags with the keyword searched, &nbsp HTML entities; 
+        function replaceTagInString(inputString){
+          const regexTag = new RegExp("<b>.*</b>", "g");
+          const regexHTML = new RegExp("&nbsp;", "g");
+          let outputString = inputString.replaceAll(regexTag, searchString);
+          outputString = outputString.replaceAll(regexHTML, "…");         
+          return outputString;
         }
+        // removes the result's date if it exists in the result
+        function removeDateInString(inputString){
+          const regex = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-]\d{2}/g;
+          const outputString = inputString.replaceAll(regex, '');
+          return outputString;
+        }
+        // extract sentence by estimating the start and end markers
+        function extractSentence(sentence){
+          let hitIndex = sentence.indexOf(searchString);
+          function findIndexMarker(string, markers, hitIndex, start=true, punctuation=false){
+            // to find the start of the sentence, find the latest marker before hitIndex
+            if(start){
+              let latestStartIndex = 0;
+              markers.forEach((marker) => {
+                let markerIndex = string.indexOf(marker) + 1; // don't include the punctuation at the start
+                latestStartIndex = ((markerIndex > latestStartIndex) && (markerIndex < hitIndex)) ? markerIndex : latestStartIndex;
+              });
+              return latestStartIndex;
+            }
+            else{ // find the earliest end of the sentence that is after the hitIndex
+              let earliestEndIndex = string.length;
+              markers.forEach((marker) => {
+                let markerIndex = punctuation ? string.indexOf(marker) + 1 : string.indexOf(marker); // include the punctuation if needed
+                earliestEndIndex = ((markerIndex < earliestEndIndex) && (markerIndex > hitIndex)) ? markerIndex : earliestEndIndex;
+              });
+              return earliestEndIndex;
+            }
+          }
+          let startMarkers = [".", ";", "。", "…", "？", "?", "|","｜","！","!","→"];
+          let endMarkers = [";","|","｜","→","【", "&"];
+          let endMarkersPunc= [".", , "。", "…", "？", "?","！","!"];
 
-        setParsedResults(parseResults(response.items));
-        console.log(response.items);
-        setLoadingSearch(false);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-    
-    
-    // check if user is logged in, and then write to firestore to save user's search
-    if(loggedIn) {
+          let startIndex = findIndexMarker(sentence, startMarkers, hitIndex, true);
+          let endIndex = findIndexMarker(sentence, endMarkers, hitIndex, false);
+          let endIndexPunc = findIndexMarker(sentence, endMarkersPunc, hitIndex, false, true);
+
+          endIndex = (endIndexPunc < endIndex) ? endIndexPunc : endIndex;
+          return {
+            text: sentence.slice(startIndex, endIndex), 
+            start: sentence.slice(startIndex, hitIndex),
+            searchedPhrase: searchString,
+            end: sentence.slice(hitIndex + searchString.length, endIndex)
+          };
+        }
+        let outputArray = [];
+        // Add text and link property from our results
+        resultsObj.forEach(resultObj => {
+          let sentenceString = '';
+          if(resultObj.htmlSnippet.includes("<b>")){
+            sentenceString = replaceTagInString(resultObj.htmlSnippet);
+              //outputArray = [...outputArray, {text: replaceTagInString(resultObj.htmlSnippet), link: resultObj.link}];
+          }
+          else if(resultObj.htmlTitle.includes("<b>")){
+            sentenceString = replaceTagInString(resultObj.htmlTitle);
+          }
+          else {
+            sentenceString = null;
+            outputArray = [...outputArray, {text: null}]
+            return;
+          }              
+          // Remove post date in sentence and extract sentence
+          sentenceString = removeDateInString(sentenceString);
+          let extractedObject = extractSentence(sentenceString);
+
+          // once w ehaver our sentence, we can split into 3 parts, phrase 1, bold keyword, phrase 2
+          const fullSentence = extractedObject.text
+          const start = extractedObject.start;
+          const searchedPhrase = extractedObject.searchedPhrase;
+          const end = extractedObject.end;
+
+          outputArray = [...outputArray, {
+            text: fullSentence, 
+            start: start, 
+            searchedPhrase: searchedPhrase, 
+            end: end, 
+            link: resultObj.link
+          }];
+        });
+        console.log(outputArray);          
+        return outputArray;
+      }
+      setParsedResults(parseResults(response.items));
+
+      formattedNumberResults = response.searchInformation.formattedTotalResults
+      setNumberResults(formattedNumberResults);
+      console.log(response.items);
+      setLoadingSearch(false);
+
+      // check if user is logged in, and then write to firestore to save user's search
+      if(loggedIn) {
         const user = firebase.auth().currentUser;
         const uniqueId = user.uid;
 
@@ -235,19 +236,29 @@ function App() {
         const time = date.getTime();
         
         const db = firebase.firestore(); // initialise your database
+        console.log('num of res from ', formattedNumberResults);
         db.collection('users').doc(uniqueId).collection('searches').add({
             text: searchString,
             searchedOn: time,
+            numResultsFormatted: formattedNumberResults,
         })
         .catch((error) => {
             console.error("Error addin doc: ", error);
-        })
-      /* ADD: code to delete once searches get past 100*/
-    }
+        })        
+        // ADD: code that adds directly to history state in local (?)
+        
+        /* ADD: code to delete once no. searches get past 10*/
+
+        retrieveHistory();
+      }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+    
+ 
      
   }
-    // Takes an array of objects (items) from google search api
-    // Returns an array with tags removed
 
   function handleChange(e) {
     setSearchInput(e.target.value);
@@ -265,8 +276,9 @@ function App() {
         </div>
       </Navbar>
       <div className='main-content'>
-        <Card parsedResults={parsedResults} loading={loadingSearch} searchInputParsed={searchInputParsed}/>
+        <Card parsedResults={parsedResults} loading={loadingSearch}/>
         <History loading={loadingHistory} loggedIn={loggedIn} history={history}/>
+        <NumberResults numberResults={numberResults}/>
       </div>
     </div>
   );
@@ -292,13 +304,23 @@ function History(props) {
   if(props.history.length !== 0){
     return (
       <div className='history'>
-        {props.history.map((search, index) => {
-          return <div key={index} className='history-item'>{search}</div>
+        <div>Recent searches</div>
+        {props.history.map((item, index) => {
+          return (<div key={index} className='history-item'>
+            <div>{item.text}</div>
+            <div>{item.numResultsFormatted}</div>
+          </div>);
         })}
       </div>
     );
   }
-  return (null);
+  return null;
+}
+
+function NumberResults(props) {
+  console.log(props.numberResults);
+  if(props.numberResults === undefined) return null;
+  return (<div>About {props.numberResults} matching sentences</div>);
 }
 
 function Navbar(props) {
@@ -322,7 +344,7 @@ function NavItem(props) {
 function NavSearchBar(props) {
   return (
     <form autoComplete="off">
-    <input type="text" maxLength="25" placeholder="Type some japanese here" 
+    <input type="text" maxLength="15" placeholder="Type some japanese here" 
     autoComplete="false" className="nav-search-bar" 
     onChange={props.handleChange}>
     </input>
